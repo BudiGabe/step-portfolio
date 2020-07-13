583015de3b9eeaea57cf256785afa743f340bf1f
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-13 15:27:35 +0000

    This is my first commit. Add a pic of me, small introduction and band generator.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random band to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomBand() {
+  const bands =
+      ['Mystic Sons', 'Taraf de Haidouks', 'Subcarpati', 'Redman'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const band = bands[Math.floor(Math.random() * bands.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const bandContainer = document.getElementById('band-container');
+  bandContainer.innerText = band;
 }
