70597f0395ba632ea5c95dd6982b996063acc49e
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-15 13:33:43 +0000

    Add generateBand function which prints random band based on mood.

--- portfolio/src/main/webapp/bandGen.js
+++ portfolio/src/main/webapp/bandGen.js
@@ -4,3 +4,14 @@ function getMood() {
     return mood;
 }
 
+function generateBand() {
+    let mood = getMood();
+    let band = [];
+    band["Chill"] = ["Elijah Nang", "Joji"];
+    band["Funky"] = ["Marc Rebillet", "TWRP"];
+    band["Ecstatic"] = ["Big Bad Voodoo Daddy", "Taraf de Haidouks"];
+    band["Fancy"] = ["Devil Doll", "The Speakeasies Swing Band"];
+    let bandContainer = document.getElementById("bandContainer");
+    let rand = Math.floor(Math.random() * Math.floor(2));
+    bandContainer.innerHTML = band[mood][rand];
+}
