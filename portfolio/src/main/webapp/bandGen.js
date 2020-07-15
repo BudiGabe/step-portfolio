842ea5c5d08e2ce412062c9916ecbe1e3d66f119
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-15 13:47:42 +0000

    Add separate function to populate the band array

--- portfolio/src/main/webapp/bandGen.js
+++ portfolio/src/main/webapp/bandGen.js
@@ -6,12 +6,17 @@ function getMood() {
 
 function generateBand() {
     let mood = getMood();
+    let band = addBands();
+    let bandContainer = document.getElementById("bandContainer");
+    let rand = Math.floor(Math.random() * Math.floor(2));
+    bandContainer.innerHTML = band[mood][rand];
+}
+
+function addBands(){
     let band = [];
     band["Chill"] = ["Elijah Nang", "Joji"];
     band["Funky"] = ["Marc Rebillet", "TWRP"];
     band["Ecstatic"] = ["Big Bad Voodoo Daddy", "Taraf de Haidouks"];
     band["Fancy"] = ["Devil Doll", "The Speakeasies Swing Band"];
-    let bandContainer = document.getElementById("bandContainer");
-    let rand = Math.floor(Math.random() * Math.floor(2));
-    bandContainer.innerHTML = band[mood][rand];
+    return band;
 }
