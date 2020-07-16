0460492b9c1942345737bfef8c3ef2e8838f9ce7
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-16 10:55:19 +0000

    Fix small nits and naming issues.

--- portfolio/src/main/webapp/bandGen.js
+++ portfolio/src/main/webapp/bandGen.js
@@ -5,15 +5,16 @@ function getMood() {
 }
 
 function generateBand() {
+    const NUMBER_OF_BANDS = 2;
     let mood = getMood();
-    let band = addBands();
+    let bands = addBands();
     let bandContainer = document.getElementById("bandContainer");
-    let rand = Math.floor(Math.random() * Math.floor(2));
+    let rand = Math.floor(Math.random() * NUMBER_OF_BANDS);
     bandContainer.innerHTML = band[mood][rand];
 }
 
-function addBands(){
-    let band = [];
+function getBands() {
+    let bands = {};
     band["Chill"] = ["Elijah Nang", "Joji"];
     band["Funky"] = ["Marc Rebillet", "TWRP"];
     band["Ecstatic"] = ["Big Bad Voodoo Daddy", "Taraf de Haidouks"];
