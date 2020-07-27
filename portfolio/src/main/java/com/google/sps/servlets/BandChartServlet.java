b69736870a8e9fd912fd195c7ae51c3fb4fceeda
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-27 17:34:09 +0000

    Fetching from genre-chart now returns a map with genres and how many votes they have

--- portfolio/src/main/java/com/google/sps/servlets/BandChartServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BandChartServlet.java
@@ -10,5 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/band-chart")
 public class BandChartServlet extends HttpServlet {
-    
+  private Map<String, Integer> bandVotes = new HashMap<>();
+  private final Gson gson = new Gson();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    String json = gson.toJson(bandVotes);
+    response.getWriter().println(json);
+  }
 }
