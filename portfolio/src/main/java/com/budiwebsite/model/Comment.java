fe2c42e22989c90b98ab910364d56f6b53a6048e
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-27 16:53:39 +0000

    Set variables to private. Comment class

--- portfolio/src/main/java/com/budiwebsite/model/Comment.java
+++ portfolio/src/main/java/com/budiwebsite/model/Comment.java
@@ -1,9 +1,13 @@
 package com.budiwebsite.model;
 
+/**
+ * Class stores comments inputted with their user along with their morality rating
+ * This allows for easier conversion from list of objects to JSON
+ */
 public class Comment {
 
-  String message;
-  double score;
+  private String message;
+  private double score;
   
   public Comment (String message, double score) {
       this.message = message;
