d1adef9daf9e9bdd28adcdef0609e1e4203adf86
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-27 17:28:22 +0000

    Create new servlet for Chart feature

---
+++ portfolio/src/main/java/com/google/sps/servlets/BandChartServlet.java
@@ -0,0 +1,14 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.HashMap;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/band-chart")
+public class BandChartServlet extends HttpServlet {
+    
+}
