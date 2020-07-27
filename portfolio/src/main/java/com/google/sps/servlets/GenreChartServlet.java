2b65cf1d5b1d0570eb18739b0983f04918371967
Budi <razvan.gabriel.budaca@gmail.com>
2020-07-27 17:47:59 +0000

    Convert from band to genre

---
+++ portfolio/src/main/java/com/google/sps/servlets/GenreChartServlet.java
@@ -0,0 +1,35 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.HashMap;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/genre-chart")
+public class GenreChartServlet extends HttpServlet {
+  private Map<String, Integer> genreVotes = new HashMap<>();
+  private final Gson gson = new Gson();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    String json = gson.toJson(genreVotes);
+    response.getWriter().println(json);
+  }
+
+   @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String genre = request.getParameter("genre");
+    int currentVotes = getCurrentVotes(genreVotes);
+    genreVotes.put(band, currentVotes + 1);
+
+    response.sendRedirect("/genre-chart.html");
+  }
+
+  public int getCurrentVotes(genreVotes) {
+      return genreVotes.containsKey(genre) ? genreVotes.get(genre) : 0;
+  }
+}
