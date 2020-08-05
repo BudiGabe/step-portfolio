1857ba154b153f0259fc689aba3a46c1d3056783
Răzvan Budacă <rbudaca@google.com>
2020-08-05 11:11:27 +0000

    Remove code that deletes overlapped events inside searchForNextEvent as it is already done

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -119,10 +119,6 @@ public final class FindMeetingQuery {
     int currEventPosition, List<TimeRange> availableTimes, MeetingRequest request) {
     // We already know that our next event is overlapped, so start from position + 2 
     for (int j = currEventPosition + 2; j < eventList.size(); j++) {
-      if (eventsOverlap(currEventTimeRange, eventList.get(j).getWhen())) {
-        eventList.remove(j);
-        j--;
-      }
       if (!eventsOverlap(currEventTimeRange, eventList.get(j).getWhen())
         && requestFits(request, currEventTimeRange.end(), eventList.get(j).getWhen().start())) {
         availableTimes.add(TimeRange.fromStartEnd(currEventTimeRange.end(), 
